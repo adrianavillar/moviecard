c1d0b6aefc7b5efc9bc1dace0a9e9c9866524694
adrianavillar <adrianavm0109@gmail.com>
2025-02-02 13:16:35 +0100

    Fix end-to-end test for Actor

--- src/test/java/com/lauracercas/moviecards/endtoendtest/ActorE2ETest.java
+++ src/test/java/com/lauracercas/moviecards/endtoendtest/ActorE2ETest.java
@@ -48,7 +48,7 @@ public class ActorE2ETest {
         assertTrue(driver.findElement(By.id("name")).isDisplayed());
         assertTrue(driver.findElement(By.id("birthDate")).isDisplayed());
         assertTrue(driver.findElement(By.id("country")).isDisplayed());
-        assertTrue(driver.findElement(By.id("deadhDate")).isDisplayed());
+        assertTrue(driver.findElement(By.id("deadDate")).isDisplayed());
 
     }
 
